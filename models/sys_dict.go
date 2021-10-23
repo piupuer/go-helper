1ea455607a44a46ea80d44a2ff5fe1926918068d
piupuer <396679132@qq.com>
2021-10-23 09:58:59 +0800

    add models

---
+++ models/sys_dict.go
@@ -0,0 +1,23 @@
+package models
+
+type SysDict struct {
+	M
+	Name      string        `gorm:"index:idx_name_unique,unique;comment:'name'" json:"name"`
+	Desc      string        `gorm:"comment:'description'" json:"desc"`
+	Status    *uint         `gorm:"type:tinyint(1);default:1;comment:'status(0: disabled, 1: enabled)'" json:"status"`
+	Remark    string        `gorm:"comment:'remark'" json:"remark"`
+	DictDatas []SysDictData `gorm:"foreignKey:DictId;comment:'one2many datas'" json:"dictDatas"`
+}
+
+type SysDictData struct {
+	M
+	Key      string  `gorm:"comment:'key'" json:"key"`
+	Val      string  `gorm:"comment:'val'" json:"val"`
+	Attr     string  `gorm:"comment:'attribute(ui input)'" json:"attr"`
+	Addition string  `gorm:"comment:'custom addition params'" json:"addition"`
+	Sort     *uint   `gorm:"comment:'sort'" json:"sort"`
+	Status   *uint   `gorm:"type:tinyint(1);default:1;comment:'status(0: disabled, 1: enabled)'" json:"status"`
+	Remark   string  `gorm:"comment:'remark'" json:"remark"`
+	DictId   uint    `gorm:"comment:'dict id'" json:"dictId"`
+	Dict     SysDict `gorm:"foreignKey:DictId" json:"dict"`
+}
---
+++ models/sys_message.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"github.com/golang-module/carbon"
+)
+
+const (
+	// message type
+	SysMessageTypeOneToOne     uint   = 0
+	SysMessageTypeOneToMany    uint   = 1
+	SysMessageTypeSystem       uint   = 2
+	SysMessageTypeOneToOneStr  string = "one2one"
+	SysMessageTypeOneToManyStr string = "one2more"
+	SysMessageTypeSystemStr    string = "system"
+
+	// message status
+	SysMessageLogStatusUnRead     uint   = 0
+	SysMessageLogStatusRead       uint   = 1
+	SysMessageLogStatusDeleted    uint   = 2
+	SysMessageLogStatusUnReadStr  string = "unread"
+	SysMessageLogStatusReadStr    string = "read"
+	SysMessageLogStatusDeletedStr string = "deleted"
+)
+
+var (
+	SysMessageTypeConst = map[uint]string{
+		SysMessageTypeOneToOne:  SysMessageTypeOneToOneStr,
+		SysMessageTypeOneToMany: SysMessageTypeOneToManyStr,
+		SysMessageTypeSystem:    SysMessageTypeSystemStr,
+	}
+	SysMessageLogStatusConst = map[uint]string{
+		SysMessageLogStatusUnRead:  SysMessageLogStatusUnReadStr,
+		SysMessageLogStatusRead:    SysMessageLogStatusReadStr,
+		SysMessageLogStatusDeleted: SysMessageLogStatusDeletedStr,
+	}
+)
+
+type SysMessage struct {
+	FromUserId uint                     `gorm:"comment:'sender user id'" json:"fromUserId"`
+	Title      string                   `gorm:"comment:'title'" json:"title"`
+	Content    string                   `gorm:"comment:'content'" json:"content"`
+	Type       uint                     `gorm:"type:tinyint;default:0;comment:'type(0: one2one, 1: one2more, 2: system(one2all))'" json:"type"`
+	RoleId     uint                     `gorm:"comment:'role id'" json:"roleId"`
+	ExpiredAt  *carbon.ToDateTimeString `gorm:"comment:'expire time'" json:"expiredAt"`
+}
+
+type SysMessageLog struct {
+	ToUserId  uint       `gorm:"comment:'receiver user id'" json:"toUserId"`
+	MessageId uint       `gorm:"comment:'message id'" json:"messageId"`
+	Message   SysMessage `gorm:"foreignKey:MessageId" json:"message"`
+	Status    uint       `gorm:"type:tinyint;default:0;comment:'status(0: unread, 1: read, 2: deleted)'" json:"status"`
+}
