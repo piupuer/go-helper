f24ddf9bbc55c1dc9806786ec56ede2771def8d3
piupuer <396679132@qq.com>
2021-10-26 18:35:18 +0800

    crypto util

---
+++ pkg/utils/crypto.go
@@ -0,0 +1,17 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// The generated password is irreversible due to the use of adaptive hash algorithm
+func GenPwd(str string) string {
+	hash, _ := bcrypt.GenerateFromPassword([]byte(str), bcrypt.DefaultCost)
+	return string(hash)
+}
+
+// By comparing two string hashes, judge whether they are from the same plaintext
+func ComparePwd(str string, pwd string) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(pwd), []byte(str)); err != nil {
+		return false
+	}
+	return true
+}
