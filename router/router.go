9e3cc9656613c369e7e2576db31c475703e02c8b
piupuer <396679132@qq.com>
2021-10-27 13:43:17 +0800

    bug fix

--- router/router.go
+++ router/router.go
@@ -23,6 +23,7 @@ func NewRouter(options ...func(*Options)) *Router {
 	if ops.logger != nil {
 		ops.jwtOps = append(ops.jwtOps, middleware.WithJwtLogger(ops.logger))
 		ops.casbinOps = append(ops.casbinOps, middleware.WithCasbinLogger(ops.logger))
+		ops.idempotenceOps = append(ops.idempotenceOps, middleware.WithIdempotenceLogger(ops.logger))
 		ops.v1Ops = append(ops.v1Ops, v1.WithDbOps(
 			query.WithMysqlLogger(ops.logger),
 		))
