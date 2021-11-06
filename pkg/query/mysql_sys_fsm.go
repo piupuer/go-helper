100351a40ca33ce83356ede4428cd52e81a62062
piupuer <396679132@qq.com>
2021-11-06 17:09:16 +0800

    bug fix

--- pkg/query/mysql_sys_fsm.go
+++ pkg/query/mysql_sys_fsm.go
@@ -8,43 +8,66 @@ import (
 
 // find finite state machine
 func (my MySql) FindFsm(r *req.FsmMachine) ([]resp.FsmMachine, error) {
-	f := fsm.New(my.Tx)
+	f := fsm.New(
+		my.Tx,
+		fsm.WithCtx(my.Ctx),
+	)
 	return f.FindMachine(r)
 }
 
 // find waiting approve log
 func (my MySql) FindFsmApprovingLog(r *req.FsmPendingLog) ([]resp.FsmApprovingLog, error) {
-	f := fsm.New(my.Tx)
+	f := fsm.New(
+		my.Tx,
+		fsm.WithCtx(my.Ctx),
+	)
 	return f.FindPendingLogByApprover(r)
 }
 
 // find waiting approve log
 func (my MySql) FsmApproveLog(r req.FsmApproveLog) (*resp.FsmApprovalLog, error) {
-	f := fsm.New(my.Tx, fsm.WithTransition(my.ops.fsmTransition))
+	f := fsm.New(
+		my.Tx,
+		fsm.WithCtx(my.Ctx),
+		fsm.WithTransition(my.ops.fsmTransition),
+	)
 	return f.ApproveLog(r)
 }
 
 func (my MySql) FsmCheckEditLogDetailPermission(r req.FsmCheckEditLogDetailPermission) error {
-	f := fsm.New(my.Tx)
+	f := fsm.New(
+		my.Tx,
+		fsm.WithCtx(my.Ctx),
+	)
 	return f.CheckEditLogDetailPermission(r)
 }
 
 // create finite state machine
 func (my MySql) CreateFsm(r req.FsmCreateMachine) error {
-	f := fsm.New(my.Tx)
+	f := fsm.New(
+		my.Tx,
+		fsm.WithCtx(my.Ctx),
+	)
 	_, err := f.CreateMachine(r)
 	return err
 }
 
 // update finite state machine
 func (my MySql) UpdateFsmById(id uint, r req.FsmUpdateMachine) error {
-	f := fsm.New(my.Tx, fsm.WithTransition(my.ops.fsmTransition))
+	f := fsm.New(
+		my.Tx,
+		fsm.WithCtx(my.Ctx),
+		fsm.WithTransition(my.ops.fsmTransition),
+	)
 	_, err := f.UpdateMachineById(id, r)
 	return err
 }
 
 // delete finite state machine
 func (my MySql) DeleteFsmByIds(ids []uint) error {
-	f := fsm.New(my.Tx)
+	f := fsm.New(
+		my.Tx,
+		fsm.WithCtx(my.Ctx),
+	)
 	return f.DeleteMachineByIds(ids)
 }
