3c99db441ec7728f163c33cd379341d8af4d0d47
piupuer <396679132@qq.com>
2022-01-06 17:20:39 +0800

    bug fix

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -2,14 +2,32 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/piupuer/go-helper/pkg/utils"
 	"net/http"
+	"strings"
 )
+
+var methods = []string{
+	http.MethodOptions,
+	http.MethodGet,
+	http.MethodPost,
+	http.MethodPut,
+	http.MethodPatch,
+	http.MethodDelete,
+}
+
+var methodStr = strings.Join(methods, ", ")
 
 func Cors(c *gin.Context) {
 	method := c.Request.Method
+	if !utils.Contains(methods, method) {
+		c.Status(http.StatusMethodNotAllowed)
+		c.Abort()
+		return
+	}
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Headers", "Content-Type, AccessToken, X-CSRF-Token, Authorization, Token, api-idempotence-token")
-	c.Header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE, OPTIONS")
+	c.Header("Access-Control-Allow-Methods", methodStr)
 	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 	c.Header("Access-Control-Allow-Credentials", "true")
 
