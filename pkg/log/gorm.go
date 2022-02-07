cc7e31accdf974ab818b00987ba861b0961671a2
piupuer <396679132@qq.com>
2022-02-07 17:24:48 +0800

    gorm logger bug fix

--- pkg/log/gorm.go
+++ pkg/log/gorm.go
@@ -13,38 +13,38 @@ import (
 
 type gormLogger struct {
 	Config
-	normalStr, traceStr, traceErrStr, traceWarnStr string
+	normalStr, normalErrStr, slowStr, slowErrStr string
 }
 
 func NewDefaultGormLogger() logger.Interface {
 	return NewGormLogger(Config{
 		gorm: logger.Config{
-			SlowThreshold: 200,
+			SlowThreshold: 200 * time.Millisecond,
 		},
 	})
 }
 
 func NewGormLogger(config Config) logger.Interface {
 	var (
-		normalStr    = "%v%s "
-		traceStr     = "%v%s\n[%.3fms] [rows:%v] %s"
-		traceWarnStr = "%v%s %s\n[%.3fms] [rows:%v] %s"
-		traceErrStr  = "%v%s %s\n[%.3fms] [rows:%v] %s"
+		normalStr    = "[%.3fms] [rows:%v] %s"
+		slowStr      = "[%.3fms(slow)] [rows:%v] %s"
+		normalErrStr = "%s\n[%.3fms] [rows:%v] %s"
+		slowErrStr   = "%s\n[%.3fms(slow)] [rows:%v] %s"
 	)
 
 	if config.gorm.Colorful {
-		normalStr = logger.Cyan + "%v" + logger.Blue + "%s " + logger.Reset
-		traceStr = logger.Cyan + "%v" + logger.Blue + "%s\n" + logger.Reset + logger.Yellow + "[%.3fms] " + logger.BlueBold + "[rows:%v]" + logger.Reset + " %s"
-		traceWarnStr = logger.Cyan + "%v" + logger.Blue + "%s " + logger.Yellow + "%s\n" + logger.Reset + logger.RedBold + "[%.3fms] " + logger.Yellow + "[rows:%v]" + logger.Magenta + " %s" + logger.Reset
-		traceErrStr = logger.Cyan + "%v" + logger.RedBold + "%s " + logger.MagentaBold + "%s\n" + logger.Reset + logger.Yellow + "[%.3fms] " + logger.BlueBold + "[rows:%v]" + logger.Reset + " %s"
+		normalStr = logger.Green + "[%.3fms] " + logger.Reset + logger.BlueBold + "[rows:%v]" + logger.Reset + " %s"
+		slowStr = logger.Yellow + "[%.3fms(slow)] " + logger.Reset + logger.BlueBold + "[rows:%v]" + logger.Reset + " %s"
+		normalErrStr = logger.RedBold + "%s\n" + logger.Reset + logger.Green + "[%.3fms] " + logger.Reset + logger.BlueBold + "[rows:%v]" + logger.Reset + " %s"
+		slowErrStr = logger.RedBold + "%s\n" + logger.Reset + logger.Yellow + "[%.3fms(slow)] " + logger.Reset + logger.BlueBold + "[rows:%v]" + logger.Reset + " %s"
 	}
 
 	l := gormLogger{
 		Config:       config,
 		normalStr:    normalStr,
-		traceStr:     traceStr,
-		traceWarnStr: traceWarnStr,
-		traceErrStr:  traceErrStr,
+		slowStr:      slowStr,
+		normalErrStr: normalErrStr,
+		slowErrStr:   slowErrStr,
 	}
 	return &l
 }
@@ -69,9 +69,9 @@ func (l gormLogger) Info(ctx context.Context, format string, args ...interface{}
 	if l.gorm.LogLevel >= logger.Info {
 		lineNum := removePrefix(utils.FileWithLineNum(), fileWithLineNum(), l.ops)
 		log := l.getLogger(ctx).WithFields(map[string]interface{}{
-			"lineNum": lineNum,
+			constant.LogLineNumKey: lineNum,
 		})
-		log.Logf(InfoLevel, l.normalStr+format, args...)
+		log.Logf(InfoLevel, format, args...)
 	}
 }
 
@@ -79,9 +79,9 @@ func (l gormLogger) Warn(ctx context.Context, format string, args ...interface{}
 	if l.gorm.LogLevel >= logger.Warn {
 		lineNum := removePrefix(utils.FileWithLineNum(), fileWithLineNum(), l.ops)
 		log := l.getLogger(ctx).WithFields(map[string]interface{}{
-			"lineNum": lineNum,
+			constant.LogLineNumKey: lineNum,
 		})
-		log.Logf(WarnLevel, l.normalStr+format, args...)
+		log.Logf(WarnLevel, format, args...)
 	}
 }
 
@@ -89,9 +89,9 @@ func (l gormLogger) Error(ctx context.Context, format string, args ...interface{
 	if l.gorm.LogLevel >= logger.Error {
 		lineNum := removePrefix(utils.FileWithLineNum(), fileWithLineNum(), l.ops)
 		log := l.getLogger(ctx).WithFields(map[string]interface{}{
-			"lineNum": lineNum,
+			constant.LogLineNumKey: lineNum,
 		})
-		log.Logf(ErrorLevel, l.normalStr+format, args...)
+		log.Logf(ErrorLevel, format, args...)
 	}
 }
 
@@ -106,16 +106,19 @@ func (l gormLogger) Trace(ctx context.Context, begin time.Time, fc func() (strin
 			row = fmt.Sprintf("%d", rows)
 		}
 		log := l.getLogger(ctx).WithFields(map[string]interface{}{
-			"lineNum": lineNum,
+			constant.LogLineNumKey: lineNum,
 		})
 		switch {
 		case l.gorm.LogLevel >= logger.Error && err != nil && !errors.Is(err, gorm.ErrRecordNotFound):
-			log.Logf(TraceLevel, l.traceErrStr, err, elapsedF, row, sql)
+			if l.gorm.SlowThreshold > 0 && elapsed > l.gorm.SlowThreshold {
+				log.Logf(ErrorLevel, l.slowErrStr, err, elapsedF, row, sql)
+			} else {
+				log.Logf(ErrorLevel, l.normalErrStr, err, elapsedF, row, sql)
+			}
 		case l.gorm.LogLevel >= logger.Warn && l.gorm.SlowThreshold > 0 && elapsed > l.gorm.SlowThreshold:
-			slowLog := fmt.Sprintf("SLOW SQL >= %v", 200)
-			log.Logf(TraceLevel, l.traceErrStr, err, slowLog, elapsedF, row, sql)
+			log.Logf(WarnLevel, l.slowStr, elapsedF, row, sql)
 		case l.gorm.LogLevel == logger.Info:
-			log.Logf(TraceLevel, l.traceErrStr, err, elapsedF, row, sql)
+			log.Logf(InfoLevel, l.normalStr, elapsedF, row, sql)
 		}
 	}
 }
