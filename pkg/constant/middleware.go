b1adf738d0f695ef7508ed3710a02c7500929c56
piupuer <396679132@qq.com>
2022-04-14 13:49:38 +0800

    bug fix

--- pkg/constant/middleware.go
+++ pkg/constant/middleware.go
@@ -28,7 +28,7 @@ const (
 	MiddlewareParamsRespCtxKey               = "ResponseBody"
 	MiddlewareParamsRespLogKey               = "Resp"
 	MiddlewareCorsOrigin                     = "*"
-	MiddlewareCorsHeaders                    = "Content-Type,AccessToken,X-CSRF-Token,Authorization,Token,api-idempotence-token"
+	MiddlewareCorsHeaders                    = "Content-Type,AccessToken,X-CSRF-Token,Authorization,Token,X-Sign-Token,api-idempotence-token"
 	MiddlewareCorsMethods                    = "OPTIONS,GET,POST,PUT,PATCH,DELETE"
 	MiddlewareCorsExpose                     = "Content-Length,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Content-Type"
 	MiddlewareCorsCredentials                = "true"
